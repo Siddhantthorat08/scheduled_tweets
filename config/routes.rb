2d3ed430a6f4d66d21a5dc28c238b1fd97c2f80b
SiddhantThorat08 <siddhant08thorat10@gmail.com>
2021-11-18 05:47:13 -0500

    Added Sign inview and performed authentication

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
 
   get "sign_up", to: "registration#new"
   post "sign_up",to: "registration#create"
+
+  get "sign_in", to: "session#new"
+  post "sign_in",to: "session#create"
+
+  delete "logout", to: "session#destroy"
   # For detaaboutils on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
