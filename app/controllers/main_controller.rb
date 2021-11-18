c5138d3f9b6c0f68787a4f1642ce4ea172107b66
SiddhantThorat08 <siddhant08thorat10@gmail.com>
2021-11-18 03:20:46 -0500

    adding session feature in regestration controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,8 @@
 class MainController < ApplicationController
 	def index
-		flash[:notice] = "Logged in Successfully"
-		flash[:alert] = "Invalid email or password"
+		if session[:user_id]
+			@user = User.find_by(id: session[:user_id])
+		end
+		
 	end
 end
